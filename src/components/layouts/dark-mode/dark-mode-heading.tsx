4cab1be7877449183418ca9123cad271af89eb41
ikhlasdansantai <initesting5@gmail.com>
2025-10-01 11:30:36 +0700

    feat(ui): add dark-mode-support section component for main landing page

---
+++ src/components/layouts/dark-mode/dark-mode-heading.tsx
@@ -0,0 +1,14 @@
+import Heading from "@/components/ui/Heading";
+
+export default function DarkModeHeading() {
+  return (
+    <div className="titles max-w-lg mx-auto text-center relative mt-4">
+      <Heading>Darkmode Support? Yes sir!</Heading>
+      <p className="mt-4">
+        IsoneUI comes with a fully well-scaled default dark theme that you can
+        apply to your application with just adding the dark attribute to your
+        page.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/sections/dark-mode-support.tsx
@@ -0,0 +1,11 @@
+import DarkModeHeading from "@/components/layouts/dark-mode/dark-mode-heading";
+import ShowCompare from "@/components/show-compare";
+
+export default function DarkModeSupport() {
+  return (
+    <section className="max-w-5xl mx-auto p-4 mt-20 sm:mt-40">
+      <DarkModeHeading />
+      <ShowCompare />
+    </section>
+  );
+}
