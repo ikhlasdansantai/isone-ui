33a0694a656b1e93189533cfe951c2cd8bb00d3d
ikhlasdansantai <initesting5@gmail.com>
2025-10-03 11:27:54 +0700

    feat: adjusting blog-container component

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,18 @@
+import BlogCard from "@/components/blog-card";
+import Heading from "@/components/ui/Heading";
+import { getPosts } from "@/lib/get-post";
+
+export default function BlogsPage() {
+  const getPostAll = getPosts();
+
+  return (
+    <main className="px-4 max-w-6xl mx-auto pt-12 pb-30 sm:py-38">
+      <section>
+        <Heading className="text-2xl">Iqra Millea</Heading>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 w-full mt-20 gap-10 gap-y-16">
+          <BlogCard data={getPostAll} />
+        </div>
+      </section>
+    </main>
+  );
+}
--- src/components/layouts/cta/cta-blogs.tsx
+++ src/components/layouts/cta/cta-blogs.tsx
@@ -4,7 +4,7 @@ import { getPosts } from "@/lib/get-post";
 export default function CtaBlogs() {
   const getPostAll = getPosts();
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 w-full mt-10 gap-6">
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 w-full mt-10 gap-10 gap-y-16">
       <BlogCard data={getPostAll} />
     </div>
   );
