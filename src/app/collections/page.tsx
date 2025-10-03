42335c77857bacf0bb0783920ea79659b75cad99
ikhlasdansantai <initesting5@gmail.com>
2025-10-03 11:39:42 +0700

    feat(page): add collections view

---
+++ src/app/collections/page.tsx
@@ -0,0 +1,17 @@
+import CollectionCards from "@/components/collection-cards";
+import Heading from "@/components/ui/Heading";
+
+export default function Page() {
+  return (
+    <main className="px-4 max-w-6xl mx-auto pt-12 pb-30 sm:pt-38">
+      <section>
+        <div className="titles max-w-2xl">
+          <Heading>
+            Collections that we've built with TailwindCSS and Luv
+          </Heading>
+        </div>
+        <CollectionCards />
+      </section>
+    </main>
+  );
+}
