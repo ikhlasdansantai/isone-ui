11379ff1fab3bb05cdf63ea349833ccb33422823
ikhlasdansantai <initesting5@gmail.com>
2025-10-03 16:34:26 +0700

    feat(ui): add footer component

---
+++ src/components/sections/footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="bg-background/85 border-t pb-22 sm:p-0">
+      <div className="max-w-3xl flex items-center justify-between mx-auto p-4 py-8">
+        <div className="space-y-2.5">
+          <p className="text-muted-foreground font-semibold text-lg">
+            isoneui 🚀
+          </p>
+          <p className="text-muted-foreground text-sm">
+            Building in <b className="text-primary">Bandung</b> by
+            ikhlasdansantai
+          </p>
+        </div>
+        <p className="text-muted-foreground">2025</p>
+      </div>
+    </footer>
+  );
+}
