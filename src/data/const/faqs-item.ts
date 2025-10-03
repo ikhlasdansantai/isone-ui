8d5fe6ff9d9aefd084d97b5a7e50ef9452a497f1
ikhlasdansantai <initesting5@gmail.com>
2025-10-03 11:12:18 +0700

    feat: adjusting static constant data

---
+++ src/data/const/faqs-item.ts
@@ -0,0 +1,14 @@
+import { FaqsProps } from "@/data/types/faqs-item.interface";
+
+export const FAQS_ITEM: FaqsProps[] = [
+  {
+    question: "What is Isone UI",
+    answer:
+      "Isone UI is a React-based UI library designed to help developers build beautiful and interactive user interfaces with ease. It offers a wide range of customizable components.",
+  },
+  {
+    question: "How do i get credit to my own contribution?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt voluptate dicta quo officiis explicabo consequuntur distinctio corporis earum similique!",
+  },
+];
