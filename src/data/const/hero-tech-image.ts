70ff884e916e86b967ab4a52a1a8498f21b84eb8
ikhlasdansantai <initesting5@gmail.com>
2025-10-06 13:13:02 +0700

    fix: relocate hero-tech-image const data  for better structure

---
+++ src/data/const/hero-tech-image.ts
@@ -0,0 +1,6 @@
+export const HERO_TECH_IMAGE: string[] = [
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg",
+];
